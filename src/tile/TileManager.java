55b6d0d536ef79eecf3ac3e88a12c3df7a848fa2
AnjalBamanu10 <anjalbamanu@gmail.com>
2023-09-09 14:22:05 +0545

    Tiles updated

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -16,15 +16,16 @@ public class TileManager {
     int mapTileNum[][];
 
 
-    public  TileManager(GamePanel gp){
+    public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
         getTileImage();
         loadMap("/maps/map01.txt");
     }
-    public void getTileImage(){
-        try{
+
+    public void getTileImage() {
+        try {
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/grass.png")));
 
@@ -33,64 +34,54 @@ public class TileManager {
 
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/water.png")));
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void  loadMap(String filePath){
-        try{
+
+    public void loadMap(String filePath) {
+        try {
             InputStream is = getClass().getResourceAsStream(filePath);
             assert is != null;
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            int col = 0;
-            int row = 0 ;
+            int row = 0;
 
-            while(col < gp.maxScreenCol && row < gp.maxScreenRow){
-                String line = br.readLine();
-
-                while(col < gp.maxScreenCol) {
-                    String[] numbers = line.split("");
-                    int num = 0;
-                    try {
-                        num = Integer.parseInt(numbers[col]);
-                        // Use the parsed integer (num) here
-                        System.out.println("The parsed integer is: " + num);
-                    } catch (NumberFormatException e) {
-                        // Handle the exception if the string cannot be parsed as an integer
-                        System.err.println("Error: The string cannot be parsed as an integer.");
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] numbers = line.split(" ");
+                for (int col = 0; col < gp.maxScreenCol && col < numbers.length; col++) {
+                    if (!numbers[col].isEmpty()) { // Check for empty strings
+                        int num = 0;
+                        try {
+                            num = Integer.parseInt(numbers[col]);
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                        }
+                        mapTileNum[col][row] = num;
                     }
-                    mapTileNum[col][row] = num;
-                    col++;
                 }
-                if(col == gp.maxScreenCol){
-                    col = 0;
-                    row ++;
-                }
+                row++;
             }
             br.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void draw(Graphics2D g2){
-        int col = 0;
-        int row = 0;
+
+    public void draw(Graphics2D g2) {
         int x = 0;
         int y = 0;
 
-        while (col < gp.maxScreenCol && row < gp.maxScreenRow){
-            int tileNum = mapTileNum[col][row];
-            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
-            col++;
-            x += gp.tileSize;
-
-            if(col == gp.maxScreenCol){
-                col = 0;
-                x = 0;
-                row++;
-                y += gp.tileSize;
+        for (int row = 0; row < gp.maxScreenRow; row++) {
+            for (int col = 0; col < gp.maxScreenCol; col++) {
+                int tileNum = mapTileNum[col][row];
+                g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+                x += gp.tileSize;
             }
+            x = 0; // Reset x to the left side for the next row
+            y += gp.tileSize;
         }
     }
 }
+
